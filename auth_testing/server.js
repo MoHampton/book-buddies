489def102c15534a662af1e438b5d95258d2a464
Andy Bonner <abonner@duke.edu>
2017-09-09 12:54:44 -0400

    move file

--- auth_testing/server.js
+++ auth_testing/server.js
@@ -1,9 +1,11 @@
 // set up Firebase tools
 var admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("./config/book-buddies-b7e5a-firebase-adminsdk-qya3g-ac91129f5a.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://book-buddies-b7e5a.firebaseio.com"
 });
+
+console.log("ok")
