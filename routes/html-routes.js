c896bdec7fa55bae3df1b46999d470cc9ce31932
Andy Bonner <abonner@duke.edu>
2017-09-13 20:21:11 -0400

    ideas for routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,4 +4,9 @@ module.exports = function(app) {
   // on get '/dashboard' render contents of bookclubs.html, w differentiation for users in clubs / not in clubs
 
   // on get '/club:id' render contents of club.html, customized w data from individual club, as determined by req.params.id
+  
+  // other views:
+  // signup / sign in / sign out--are the signin / signup forms two different views that get injected into the modal?
+  // On signout, what changes?
+  // Are there areas of index.hbs that change based on signin/signout?
 }
