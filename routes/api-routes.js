38b5f2f0f84de6b04623cf21c724bbe121ef06a6
Andy Bonner <abonner@duke.edu>
2017-09-15 15:00:56 -0400

    change to findOrCreate in case Association already made

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,7 +11,11 @@ module.exports = function(app) {
   // PUT route to add a user to a club
   app.post("/join/:ClubId/:UserId", function(req, res) {
     console.log("req.params:", req.params);
-    db.Association.create(req.params);
+    // e.g. {ClubId: '1', UserId: '11'}
+    db.Association.findOrCreate({
+      // creates association if it doesn't already exist
+      where: req.params
+    });
   });
 
   app.post("/join/:ClubId", function(req, res) {
